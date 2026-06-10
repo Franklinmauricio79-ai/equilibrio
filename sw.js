0ec19b9a73553b2d2f8ac29aa0bd653fcb96460b
Franklinmauricio79-ai <franklinmauricio79@gmail.com>
2026-06-09 23:33:08 -0500

    Koa 3D: cara visible (opacidad, luces, tone mapping) (sw.js)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Equilibrio · Service Worker */
-var CACHE = 'equilibrio-v5';
+var CACHE = 'equilibrio-v6';
 var ASSETS = ['./', './index.html', './manifest.json', './GLTFLoader.js', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', function (e) {
